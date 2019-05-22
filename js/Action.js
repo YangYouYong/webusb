c3e74801afdd95a7e34362426b5ec5d40936bcb7
yangyouyong <youyong.yang@vechain.com>
2019-05-22 15:35:03 +0800

    Initialize

---
+++ js/Action.js
@@ -0,0 +1,17 @@
+$(document).ready(function(){
+
+    $("div.container").click(function(){
+        USBDevice.getDevice().then((devices) => {
+            console.log("decetor");
+            console.log(devices);
+        });
+        console.log(" clicked ");
+    });
+
+    let devices = await navigator.usb.getDevices();
+    devices.forEach(device => {
+        // Add |device| to the UI.
+        console.log("await navigator.usb.getDevices()");
+        console.log(device);
+    });
+});
